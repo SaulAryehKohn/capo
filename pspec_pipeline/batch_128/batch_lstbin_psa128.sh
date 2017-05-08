720da23a23d16d0741cd1f0741b73c3343f8921b
SaulAryehKohn <saul.aryeh.kohn@gmail.com>
2017-05-08 15:33:24 -0400

    fixed small errors in batch_lstbin

--- pspec_pipeline/batch_128/batch_lstbin_psa128.sh
+++ pspec_pipeline/batch_128/batch_lstbin_psa128.sh
@@ -3,18 +3,17 @@
 #$ -cwd
 #$ -o grid_output
 #$ -j y
-#$ -l h_vmem=18G
+#$ -l h_vmem=12G
 #$ -N LSTBIN
 export PATH=/home/saulkohn/src/anaconda/bin:$PATH
 source activate PAPER
-LSTS=`python -c "import numpy as n; hr = 1.0; print ' '.join(['%f_%f' % (d,d+hr/4.) for d in n.arange(0,23.999*hr,hr/4.)])"`
-#LSTS=`python -c "import numpy as n; hr = 1.0; print ' '.join(['%f_%f' % (d,d+hr/4.) for d in n.arange(6,9*hr,hr/4.)])"`
-#LSTS=`python -c "import numpy as n; hr = 1.0; print ' '.join(['%f_%f' % (d,d+hr/4.) for d in n.arange(5.25,9.5*hr,hr/4.)])"`
-#LSTS=9.250000_9.500000
+
+LSTS=`python -c "import numpy as n; hr = 1.0; print ' '.join(['%f_%f' % (d,d+hr/4.) for d in n.arange(0,23.999*hr,hr/4.)])"` # 96 lst bins from 0-24hr
 MY_LSTS=`pull_args.py $LSTS`
-CALFILE=psa6622_v002
-PREFIX=lstbin_Apr12_V_fgsub
+CALFILE=psa6622_v003
+PREFIX=lstbin_May08_I_fg
 PATH2CAPO=/home/saulkohn/ReposForCanopy/capo
+
 echo $MY_LSTS
 echo mkdir ${PREFIX}
 mkdir ${PREFIX}
@@ -24,18 +23,18 @@ for LST in $MY_LSTS; do
     echo working on even files
     mkdir even
     cd even
-    echo ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=42.95 --lst_rng=$LST \
+    echo ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=31.65 --lst_rng=$LST \
     --tfile=600 --altmax=0 --stats=all --median --nsig=3 `python ${PATH2CAPO}/scripts/select_file_parity.py $*`
-    python ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE}  --lst_res=42.95 --lst_rng=$LST \
+    python ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE}  --lst_res=31.65 --lst_rng=$LST \
     --tfile=600 --altmax=0 --stats=all --median --nsig=3 `python ${PATH2CAPO}/scripts/select_file_parity.py $*`
     cd ..
 
     echo working on odd files
     mkdir odd
     cd odd
-    echo ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=42.95 --lst_rng=$LST \
-    --tfile=600 --altmax=0 --stats=all --median --nsig=3 `python ~/scripts/select_file_parity.py $*`
-    python ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=42.95 --lst_rng=$LST \
+    echo ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=31.65 --lst_rng=$LST \
+    --tfile=600 --altmax=0 --stats=all --median --nsig=3 `python ${PATH2CAPO}/scripts/select_file_parity.py $*`
+    python ${PATH2CAPO}/scripts/lstbin_v02.py -a cross -C ${CALFILE} --lst_res=31.65 --lst_rng=$LST \
     --tfile=600 --altmax=0 --stats=all --median --nsig=3 `python ${PATH2CAPO}/scripts/select_file_parity.py --odd $*`
     cd ..
 
