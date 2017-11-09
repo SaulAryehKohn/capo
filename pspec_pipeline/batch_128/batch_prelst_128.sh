00379c21b7c2a628c21916e7723363cda74dd7b1
SaulAryehKohn <saul.aryeh.kohn@gmail.com>
2017-11-09 10:47:25 -0500

    move RFI flagging to a different pspec batch script

--- pspec_pipeline/batch_128/batch_prelst_128.sh
+++ pspec_pipeline/batch_128/batch_prelst_128.sh
@@ -6,14 +6,14 @@
 #$ -l h_vmem=5G
 #$ -N fg_sub
 
-source activate HERA
+source activate PAPER #HERA
 ARGS=`pull_args.py $*`
 CALFILE=psa6622_v003
 PATH2CAPO='/home/saulkohn/ReposForCanopy/capo'
 
 for FILE in $ARGS; do
-    echo ${PATH2CAPO}/pspec_pipeline/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-9 --horizon=15 ${FILE}
-    ${PATH2CAPO}/pspec_pipeline/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-9 --horizon=15 ${FILE}
+    echo ${PATH2CAPO}/pspec_pipeline/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-12 --horizon=15 ${FILE}
+    ${PATH2CAPO}/pspec_pipeline/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-12 --horizon=15 ${FILE}
 
     echo xrfi_simple.py -n 4 ${FILE}B
     xrfi_simple.py -n 4 ${FILE}B #&& rm ${FILE}B/* && rmdir ${FILE}B
