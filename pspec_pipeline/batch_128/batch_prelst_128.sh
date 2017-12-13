34d099c2d1c5d7e6b715c2a679d1f1cb920a3c03
SaulAryehKohn <saul.aryeh.kohn@gmail.com>
2017-12-13 14:25:59 -0500

    update batch_prelst_128

--- pspec_pipeline/batch_128/batch_prelst_128.sh
+++ pspec_pipeline/batch_128/batch_prelst_128.sh
@@ -6,7 +6,7 @@
 #$ -l h_vmem=5G
 #$ -N fg_flag_sub_linclean
 
-source activate PAPER #HERA
+source activate PAPER
 ARGS=`pull_args.py $*`
 CALFILE=psa6622_v003
 PATH2CAPO='/home/saulkohn/ReposForCanopy/capo'
@@ -16,8 +16,8 @@ for FILE in $ARGS; do
     #${PATH2CAPO}/pspec_pipeline/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-12 --horizon=15 ${FILE}
     echo xrfi_simple.py -n 1000 -c 0_42,50_51,64_65,72_78,100_103,151_154,166_170,175,178_202 ${FILE}
     xrfi_simple.py -n 1000 -c 0_42,50_51,64_65,72_78,100_103,151_154,166_170,175,178_202 ${FILE}
-    echo python ${PATH2CAPO}/sak/scripts/linclean.py -C ${CALFILE} --horizon=15 ${FILE}R
-    python ${PATH2CAPO}/sak/scripts/linclean.py -C ${CALFILE} --horizon=15 ${FILE}R
+    echo python ${PATH2CAPO}/sak/scripts/linclean.py --horizon=15 ${FILE}R
+    python ${PATH2CAPO}/sak/scripts/linclean.py --horizon=15 ${FILE}R
     echo xrfi_simple.py -n 4 ${FILE}RB
     xrfi_simple.py -n 4 ${FILE}RB #&& rm ${FILE}B/* && rmdir ${FILE}B
 done
